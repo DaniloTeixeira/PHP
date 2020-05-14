ba8d731166ccadfa15f81f834530b7348d107399
Danilo <danilo.rt@outlook.com>
2020-05-13 21:43:05 -0300

    Refatorando viaCep

--- viacep.php
+++ viacep.php
@@ -1,24 +1,47 @@
 <?php 
 
-$address = (object) [
-    'cep' => '',
-    'logradouro' => '',
-    'bairro' => '',
-    'localidade' => '',
-    'uf' => ''
-];
+function getAddress () {
 
-if (isset($_POST['cep'])){
-    $cep = $_POST['cep'];
+    if (isset($_POST['cep'])){
+        $cep = $_POST['cep'];
 
-    $cep = preg_replace('/[^0-9]/','',$cep);
-    
-    if (preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep)) {
-        $url = "https://viacep.com.br/ws/{$cep}/json/";
-
-        $address = json_decode(file_get_contents($url));
-    } else{
-        $address->cep = 'CEP inválido';
+        $cep = filterCep ($cep);
+        
+        if (isCep ($cep)) {
+            $address = getAdressViaCep($cep);
+        } else {
+            $address = addressEmpty();
+            $address->cep = 'CEP inválido';
+        }
+    } else {
+            $address = addressEmpty();
     }
     
+    return $address;
 }
+
+function addressEmpty () {
+    return (object) [
+        'cep' => '',
+        'logradouro' => '',
+        'bairro' => '',
+        'localidade' => '',
+        'uf' => ''
+    ];
+}
+
+function filterCep (String $cep):String {
+    return preg_replace('/[^0-9]/','',$cep);
+}
+
+function isCep (String $cep):bool {
+    return preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep);
+}
+
+function getAdressViaCep (String $cep) {
+    $url = "https://viacep.com.br/ws/{$cep}/json/";
+    return json_decode(file_get_contents($url));
+}
+
+// refatorando o código
+// expressoes regulares
